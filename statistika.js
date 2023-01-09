7ce807533eadc23263d770b33305dfbba8f5a7da
Dragan Boscanin <dboscanin@gmail.com>
2023-01-09 22:44:26 +0100

    Statistika

---
+++ statistika.js
@@ -0,0 +1,24 @@
+
+function statistika() {
+  let fi = prompt("Unesi vrednost fi")
+  let xi = prompt("Unesi vrednost xi")
+  let xbar = prompt("Unesi vrednost xbar")
+  let rezultat = (fi*Math.pow((xi - xbar),2))
+  fi = Number
+  xi = Number
+  xbar = Number
+ 
+
+  return rezultat
+
+}
+
+document.write(statistika())
+
+
+
+
+
+
+
+
